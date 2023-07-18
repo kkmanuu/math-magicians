3477ee7b17cba2a213f4e6e39982ba659f8d410e
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-18 22:08:42 +0300

    chore:  make one child component and update my calculator design

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,52 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Calculator = () => {
-  const [state, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-  function handleClick(buttonName) {
-    const newState = Calculator(state, buttonName);
-    setState(newState);
-  }
-  return (
-    <div className="calculator">
-      <div className="buttons-div">
-        <div className="input-field">{state.next || state.total || '0'}</div>
-      </div>
-      <div className="buttons-div">
-        <button type="button" onClick={() => handleClick('AC')}>AC</button>
-        <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
-        <button type="button" onClick={() => handleClick('%')}>%</button>
-        <button type="button" className="operation" onClick={() => handleClick('÷')}>÷</button>
-      </div>
-      <div className="buttons-div">
-        <button type="button" onClick={() => handleClick('7')}>7</button>
-        <button type="button" onClick={() => handleClick('8')}>8</button>
-        <button type="button" onClick={() => handleClick('9')}>9</button>
-        <button type="button" className="operation" onClick={() => handleClick('x')}>x</button>
-      </div>
-      <div className="buttons-div">
-        <button type="button" onClick={() => handleClick('4')}>4</button>
-        <button type="button" onClick={() => handleClick('5')}>5</button>
-        <button type="button" onClick={() => handleClick('6')}>6</button>
-        <button type="button" className="operation" onClick={() => handleClick('-')}>-</button>
-      </div>
-      <div className="buttons-div">
-        <button type="button" onClick={() => handleClick('1')}>1</button>
-        <button type="button" onClick={() => handleClick('2')}>2</button>
-        <button type="button" onClick={() => handleClick('3')}>3</button>
-        <button type="button" className="operation" onClick={() => handleClick('+')}>+</button>
-      </div>
-      <div className="buttons-div">
-        <button type="button" className="zero" onClick={() => handleClick('0')}>0</button>
-        <button type="button" onClick={() => handleClick('.')}>.</button>
-        <button type="button" className="operation" onClick={() => handleClick('=')}>=</button>
-      </div>
+const Calculator = () => (
+  <div className="calculator">
+    <div className="buttons-div">
+      <div className="input-field">0</div>
     </div>
-  );
-};
+    <div className="buttons-div">
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="operation">÷</button>
+    </div>
+    <div className="buttons-div">
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="operation">x</button>
+    </div>
+    <div className="buttons-div">
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="operation">-</button>
+    </div>
+    <div className="buttons-div">
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="operation">+</button>
+    </div>
+    <div className="buttons-div">
+      <button type="button" className="zero">0</button>
+      <button type="button">.</button>
+      <button type="button" className="operation">=</button>
+    </div>
+  </div>
+);
 
 export default Calculator;
---
+++ src/Components/addButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <div className="num">0</div>
+  );
+}
+
+export default Button;
