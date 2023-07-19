dcee4e4fbbfb6531ff7d0a421893ae3f82b3fc41
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-19 18:14:52 +0300

    chore:Refactor Calculator component for button rendering

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,40 +1,75 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="buttons-div">
-      <div className="input-field">0</div>
-    </div>
-    <div className="buttons-div">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="operation">÷</button>
-    </div>
-    <div className="buttons-div">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="operation">x</button>
-    </div>
-    <div className="buttons-div">
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="operation">-</button>
-    </div>
-    <div className="buttons-div">
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="operation">+</button>
-    </div>
-    <div className="buttons-div">
-      <button type="button" className="zero">0</button>
-      <button type="button">.</button>
-      <button type="button" className="operation">=</button>
-    </div>
-  </div>
+const Button = ({ label, type, handleClick }) => (
+  <button type="button" className={type} onClick={handleClick}>
+    {label}
+  </button>
 );
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    const newState = calculate(state, buttonName);
+    setState(newState);
+  };
+
+  const renderButton = (label, type) => (
+    <Button key={label} label={label} type={type} handleClick={() => handleClick(label)} />
+  );
+
+  return (
+    <div className="calculator">
+      <div className="buttons-div">
+        <div className="input-field">{state.next || state.total || '0'}</div>
+      </div>
+      <div className="buttons-div">
+        {[
+          ['AC', 'operation'],
+          ['+/-', 'operation'],
+          ['%', 'operation'],
+          ['÷', 'operation'],
+        ].map(([label, type]) => renderButton(label, type))}
+      </div>
+      <div className="buttons-div">
+        {[
+          ['7', 'number'],
+          ['8', 'number'],
+          ['9', 'number'],
+          ['x', 'operation'],
+        ].map(([label, type]) => renderButton(label, type))}
+      </div>
+      <div className="buttons-div">
+        {[
+          ['4', 'number'],
+          ['5', 'number'],
+          ['6', 'number'],
+          ['-', 'operation'],
+        ].map(([label, type]) => renderButton(label, type))}
+      </div>
+      <div className="buttons-div">
+        {[
+          ['1', 'number'],
+          ['2', 'number'],
+          ['3', 'number'],
+          ['+', 'operation'],
+        ].map(([label, type]) => renderButton(label, type))}
+      </div>
+      <div className="buttons-div">
+        {[
+          ['0', 'number zero'],
+          ['.', 'operation'],
+          ['=', 'operation'],
+        ].map(([label, type]) => renderButton(label, type))}
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
