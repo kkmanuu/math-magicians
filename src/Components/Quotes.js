6edd02f206e47320b5e97f2bdb98bcbe9aa8de50
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-25 14:58:48 +0300

    Ref:REfactor Quotes using dynamic paragraps

---
+++ src/Components/Quotes.js
@@ -0,0 +1,14 @@
+import '../styles/quotes.css';
+
+const Quotes = () => (
+  <div>
+    <div className="pDiv">
+      <p>
+        Mathematics is not about numbers and eqautions, computations
+        or algorithms: It`s about understanding. -- William Paul Thurtson
+      </p>
+    </div>
+  </div>
+);
+
+export default Quotes;
